3d2dd1bc1e8494369d7d93e9a80c8a1161ba428a
verosh <vero.shabun@gmail.com>
2020-07-27 14:33:33 +0200

    added heading to 1-2

--- chapter1/e2.c
+++ chapter1/e2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-main() {
-    int fahr, cels;
+int main() {
+    float fahr, cels;
     int low, up, step; /* this is a comment */
 
     low = 0;
@@ -10,9 +10,14 @@ main() {
 
     fahr = low;
 
+    printf( "%3s\t%6s\n\n","F","C" );
+
     while( fahr <= up ) {
-        cels = 5* (fahr - 32) / 9;
-        printf("%d\t%d\n",fahr,cels);
+        cels = (5.0/9.0) * (fahr - 32.0) ;
+        printf("%3.0f\t%6.2f\n",fahr,cels);
         fahr += step;
     }
+
+
+    return 0;
 }
