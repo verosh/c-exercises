83cdddf933eb99b3ec5fc5a9330752953fa3c25d
verosh <vero.shabun@gmail.com>
2020-07-30 15:33:07 +0200

    added exec 1-20

---
+++ chapter1/sec10-exec20.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+#define TABLEN 8
+
+int main(void){
+    int c, current, num;
+
+    num = 0;
+    current = 1;
+
+    while((c=getchar()) != EOF){
+        if (c == '\t'){
+            num = TABLEN - ((current - 1) % TABLEN);
+
+            while(num > 0){
+                putchar(' ');
+                current++;
+                num--;
+            }
+        } else if(c == '\n'){
+            putchar(c);
+            current = 1;
+        } else {
+            putchar(c);
+            current++;
+        }
+
+
+    }
+
+
+    return 0;
+}
