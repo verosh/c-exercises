692cffc816627c3284c29c4fba240d958cbf780e
verosh <vero.shabun@gmail.com>
2020-08-05 17:04:28 +0200

    made 2-1 more readable and included byte sizes

--- chapter2/sec2.c
+++ chapter2/sec2.c
@@ -3,16 +3,14 @@
 
 
 int main(void){
-    printf("Max int size: %d, %ld bytes\n",INT_MAX, sizeof(int));
-    printf("Min int size: %d\n",INT_MIN);
-    printf("Max short size: %d %ld bytes\n",SHRT_MAX, sizeof(short int));
-    printf("Min short size: %d\n",SHRT_MIN);
-    printf("Max long size: %ld\n",LONG_MAX);
-    printf("Min long size: %ld\n",LONG_MIN);
-    printf("Max char size: %d\n",CHAR_MAX);
-    printf("Min char size: %d\n",CHAR_MIN);
-    printf("%ld",sizeof(short int));
-
+    printf("Max int size:\t %10d\t\t, %ld bytes\n",INT_MAX, sizeof(int));
+    printf("Min int size:\t %10d\n",INT_MIN);
+    printf("Max short size:\t %10d\t\t, %ld bytes\n",SHRT_MAX, sizeof(short int));
+    printf("Min short size:\t %10d\n",SHRT_MIN);
+    printf("Max long size:\t %10ld\t, %ld bytes\n",LONG_MAX, sizeof(long int));
+    printf("Min long size:\t %10ld\n",LONG_MIN);
+    printf("Max char size:\t %10d\t\t, %ld bytes\n",CHAR_MAX, sizeof(char));
+    printf("Min char size:\t %10d\n",CHAR_MIN);
 
     return 0;
 }
